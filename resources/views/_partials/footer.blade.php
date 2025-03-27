2089192f2de15ae91a9388260b00653ebf8056b9
AnDraDeDS <andradevieira909@gmail.com>
2025-03-26 21:01:18 -0300

    aula 26-03-25, utilizando eloquent, migration e models

---
+++ resources/views/_partials/footer.blade.php
@@ -0,0 +1,3 @@
+<footer class="footer text-center fixed-botton">
+      <p>&copy; 2023 Adote um Amigo. Todos os direitos reservados.</p>
+    </footer>
--- resources/views/registro.blade.php
+++ resources/views/registro.blade.php
@@ -1,69 +1,4 @@
-<!doctype html>
-<html lang="pt-BR">
-  <head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <title>Registro - Adote um Amigo</title>
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
-    <style>
-      body {
-        background-color: #023535;
-        color: #ffffff;
-      }
-      .navbar {
-        background-color: #015958;
-      }
-      .btn-primary {
-        background-color: #0FC2C0;
-        border-color: #0CABA8;
-      }
-      .btn-primary:hover {
-        background-color: #0CABA8;
-        border-color: #008F8C;
-      }
-      .register-container {
-        background-color: #008F8C;
-        padding: 2rem;
-        border-radius: 0.5rem;
-        margin-top: 5rem;
-      }
-      .footer {
-        background-color: #015958;
-        padding: 1rem 0;
-        margin-top: 2rem;
-        position: fixed;
-        bottom: 0;
-        width: 100%;
-      }
-    </style>
-  </head>
-  <body>
-    <nav class="navbar navbar-expand-lg navbar-dark">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="#">Adote um Amigo</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link" href="index.html">Início</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Sobre Nós</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Animais para Adoção</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#">Contato</a>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </nav>
-
-    <div class="container">
+<div class="container">
       <div class="row justify-content-center">
         <div class="col-md-6">
           <div class="register-container">
@@ -96,11 +31,3 @@
         </div>
       </div>
     </div>
-
-    <footer class="footer text-center fixed-botton">
-      <p>&copy; 2023 Adote um Amigo. Todos os direitos reservados.</p>
-    </footer>
-
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
-  </body>
-</html>
