4f848cbd1c8af3f0784e8bf48faa239ec4848aba
AnDraDeDS <andradevieira909@gmail.com>
2025-04-09 18:15:03 -0300

    utilizando tinker(esta no controller cadastroanimal)

--- app/Http/Controllers/CadastroAnimal.php
+++ app/Http/Controllers/CadastroAnimal.php
@@ -8,8 +8,11 @@ use App\Models\Animal;
 class CadastroAnimal extends Controller
 {
     function animal(){
+ 
         return view('animal');
+
     }
+    
     function addAnimal(Request $request){
         $animal = new Animal();
         // dd($request->all());
@@ -17,4 +20,31 @@ class CadastroAnimal extends Controller
 
         return view('animal');
     }
+    // apenas tinker
+
+    function inserirAnimal(){
+        $animal = new Animal();
+
+        $animal->nome='Menina';
+        $animal->cor='Marrom';
+        $animal->peso='18kg';
+        $animal->idade='12';
+        $animal->especie='Cachorro';
+        $animal->raca='Shih Tzu';
+    }
+
+    function SelectAnimais(){
+        $animal = new Animal();
+        $animais = new Animal();
+        $animal::all();
+        // $animal::find(1); // especifico
+
+        foreach($animais as $animal){
+            echo $animal->nome;
+     
+        }
+        $animais2 = Animal::where('nome', '=', 'theo')->get();
+
+    }
+    
 }
